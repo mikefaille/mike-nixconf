27b5ae6fbbdf818367122fe517b60ec1edcae6be
Michaël Faille <michael@faille.io>
2016-12-04 00:37:59 -0500

    Inherit proxy in nix commands

--- configuration.nix
+++ configuration.nix
@@ -397,4 +397,11 @@
 
   programs.bash.enableCompletion = true;
 
+  lib.fetchers.proxyImpureEnvVars = [
+    # We borrow these environment variables from the caller to allow
+    # easy proxy configuration.  This is impure, but a fixed-output
+    # derivation like fetchurl is allowed to do so since its result is
+    # by definition pure.
+    "http_proxy" "https_proxy" "ftp_proxy" "all_proxy" "no_proxy"
+  ];
 }
